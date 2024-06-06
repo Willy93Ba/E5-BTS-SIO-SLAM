2f95995b93dfa8676e302929b92619249e61c5e9
Willy93Ba <114831455+Willy93Ba@users.noreply.github.com>
2024-06-06 14:15:22 +0200

    Update script_reception_notifications.php

--- Projet Prise de rendez-vous/Code Source/script_reception_notifications.php
+++ Projet Prise de rendez-vous/Code Source/script_reception_notifications.php
@@ -1,16 +1,1 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['nom_parent']) && isset($_POST['date_time'])) {
-    $nom_parent = $_POST['nom_parent'];
-    $date_time = $_POST['date_time'];
 
-    // Traitez la notification de rendez-vous reçue du menu parent
-    // Affichez-la ou effectuez les actions nécessaires dans le menu du professeur.
-
-    // Vous pouvez également stocker ces notifications dans une base de données pour un affichage ultérieur.
-
-    $response = ['success' => true];
-    echo json_encode($response);
-} else {
-    // Gérez le cas où la requête n'est pas de type POST
-}
-?>
